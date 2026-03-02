7ed1aadb238532812988567bb8a86f98ac893bdf
myz <xuziye0327@gmail.com>
2026-03-03 01:31:39 +0800

    ref: 重构dockerfile

--- webui/vue.config.js
+++ webui/vue.config.js
@@ -33,12 +33,12 @@ module.exports = {
     const { execSync } = require('child_process');
     const moment = require('moment');
     config.plugin('define').tap((args) => {
-      args[0]['process.env'].version = JSON.stringify({
-        updateTime: moment(execSync('git log --pretty=format:%at -1').toString().trim() * 1000).utcOffset(8).format('YYYY-MM-DD HH:mm:ss'),
-        head: execSync('git rev-parse HEAD').toString().trim().substring(0, 12),
-        commitInfo: execSync('git log --pretty=format:%s -1').toString().trim(),
-        version: execSync('git describe --tags').toString().trim().split('-')[0]
-      });
+      // args[0]['process.env'].version = JSON.stringify({
+      //   updateTime: moment(execSync('git log --pretty=format:%at -1').toString().trim() * 1000).utcOffset(8).format('YYYY-MM-DD HH:mm:ss'),
+      //   head: execSync('git rev-parse HEAD').toString().trim().substring(0, 12),
+      //   commitInfo: execSync('git log --pretty=format:%s -1').toString().trim(),
+      //   version: execSync('git describe --tags').toString().trim().split('-')[0]
+      // });
       return args;
     });
   },
